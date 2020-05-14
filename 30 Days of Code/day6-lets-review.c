db91d2ff5738664736e62e5185601fee298901be
vignesh98 <vigneshn000@gmail.com>
2020-05-14 07:08:52 +0530

    6

--- 30 Days of Code/day6-lets-review.c
+++ 30 Days of Code/day6-lets-review.c
@@ -1,12 +1,32 @@
 #include<stdio.h>
 #include<string.h>
 
-int main(){
 
+int main(){
 int a;
-scanf("%d\n",&a);
-char str[100];
-fgets(str,sizeof str,stdin);
-printf("%s",str);
+char str[10000];
+scanf("%d",&a);
+
+for(int i=0;i<a;i++){
+
+scanf("%s",&str);
+for(int x=0;x<strlen(str);x++){
+
+    if(x % 2==0){
+        printf("%c",str[x]);
+    }
+}
+            printf(" ");
+for(int y=0;y<strlen(str);y++){
+    if(y% 2!=0){
+        printf("%c",str[y]);
+    }
+}
+printf("\n");
+
+
+}
+
 return 0;
 }
+
